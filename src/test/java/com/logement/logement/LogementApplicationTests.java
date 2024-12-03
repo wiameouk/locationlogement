8bc446cbe6c7dddbade70acb3cc949ed308c1b44
wiameouk <wiamoukaili7@gmail.com>
2024-12-03 20:30:58 +0100

    Initial commit

---
+++ src/test/java/com/logement/logement/LogementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logement.logement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
